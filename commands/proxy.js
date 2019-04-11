a0acf4a26e04cdeaaa6ac9b0787fcfe90bd6b581
Prageeth Silva <prageeth.silva@cgi.com>
2019-04-11 17:21:02 +0200

    fix header rewrites

--- commands/proxy.js
+++ commands/proxy.js
@@ -18,14 +18,14 @@ const isHtmlResponse = (headers) => {
   return contentType.includes('/html');
 };
 
-const rewriteUrlInBody = (body, search, replace) => {
+const rewriteUrlInString = (body, search, replace) => {
   return (body || '').replace(search, replace);
 };
 
 const getOriginalHeaders = (rawHeaders) => {
   const headers = {};
   const length = rawHeaders ? (rawHeaders.length) || 0 : 0;
-  const key = null;
+  let key = null;
   for (let i=0; i<length; i++) {
     if (i % 2 === 0) {
       key = rawHeaders[i];
@@ -35,6 +35,23 @@ const getOriginalHeaders = (rawHeaders) => {
   }
   return headers;
 };
+
+const isRedirect = (statusCode) => {
+  return statusCode >= 301 && statusCode <= 308;
+};
+
+const rewriteLocationHeader = (originalHeaders, search, replacement) => {
+  const headers = {};
+  for (let key in originalHeaders) {
+    if (key.toLowerCase() === 'location') {
+      headers[key] = rewriteUrlInString(originalHeaders[key], search, replacement);
+    } else {
+      headers[key] = originalHeaders[key];
+    }
+  }
+  return headers;
+};
+
 
 class Proxy {
 
@@ -55,15 +72,20 @@ class Proxy {
 
     proxy.on('proxyRes', function (proxyRes, req, res) {
       let body = new Buffer('');
+      let originalHeaders = getOriginalHeaders(proxyRes.rawHeaders);
       proxyRes.on('data', function (data) {
           body = Buffer.concat([body, data]);
       });
       proxyRes.on('end', function () {
-          body = body.toString();
-          if (rewriteBodyUrls && isHtmlResponse(proxyRes.headers)) {
-            body = rewriteUrlInBody(body, search, replacement);
+          if (rewriteBodyUrls) {
+            if (isHtmlResponse(proxyRes.headers)) {
+              body = rewriteUrlInString(body.toString(), search, replacement);
+            }
+            if (isRedirect(proxyRes.statusCode)) {
+              originalHeaders = rewriteLocationHeader(originalHeaders, search, replacement);
+            }
           }
-          res.writeHead(proxyRes.statusCode, getOriginalHeaders(proxyRes.rawHeaders));
+          res.writeHead(proxyRes.statusCode, originalHeaders);
           res.end(body);
       });
     });
